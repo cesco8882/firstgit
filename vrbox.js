9248df981f1b77b3218c92720fa393dd21b013a9
Francesco Trevisani <soluzioniduepuntozero@gmail.com>
2016-06-29 10:32:34 +0200

    hi

---
+++ vrbox.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+    $('#clickhere').click(function(){
+		$('#obj1').addClass('visible');
+		$('#obj2').addClass('visible');
+		}
+	);
+	$('#clickhere1').click(function(){
+		$('#obj1').addClass('visible');
+		$('#obj2').addClass('visible');
+		}
+	);
+});
+
