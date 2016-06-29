4eea975ed781e20c469bbbff2595baeba5948aa5
cesco8882 <soluzioniduepuntozero@gmail.com>
2016-06-29 17:09:34 +0200

    Update vrbox.js
    correct formula

--- vrbox.js
+++ vrbox.js
@@ -11,3 +11,16 @@ jQuery(document).ready(function($) {
 	);
 });
 
+jQuery(document).ready(function($) {
+    $('#clickhere').click(function(){
+		$('#obj1').addClass('visible');
+		$('#obj2').addClass('visible');
+		}
+	);
+	$('#clickhere1').click(function(){
+		$('#obj1').addClass('visible');
+		$('#obj2').addClass('visible');
+		}
+	);
+});
+
